76316404901e12fca1ff53c3fa82f2458c2b2e80
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-08-22 22:35:06 -0700

    gui: add get_relative_center_location function

--- templateapp/application.py
+++ templateapp/application.py
@@ -16,6 +16,28 @@ __version__ = version
 __edition__ = edition
 
 
+def get_relative_center_location(parent, width, height):
+    """get relative a center location of parent window.
+
+    Parameters
+    ----------
+    parent (tkinter): tkinter component instance.
+    width (int): a width of a child window.
+    height (int): a height of a child window..
+
+    Returns
+    -------
+    tuple: x, y location.
+    """
+    pwh, px, py = parent.winfo_geometry().split('+')
+    px, py = int(px), int(py)
+    pw, ph = [int(i) for i in pwh.split('x')]
+
+    x = int(px + (pw - width) / 2)
+    y = int(py + (ph - height) / 2)
+    return x, y
+
+
 class Application:
     def __init__(self):
         self._base_title = 'Template GUI'
