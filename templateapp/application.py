5543edbfb2407c089ac1fe750e24f5a972f99dd4
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-08-22 22:41:01 -0700

    gui: add Data class

--- templateapp/application.py
+++ templateapp/application.py
@@ -4,10 +4,9 @@ import tkinter as tk
 # from tkinter import ttk
 # from tkinter import filedialog
 from tkinter import messagebox
-# from os import path
-# from pprint import pformat
+from os import path
 # import webbrowser
-# from textwrap import dedent
+from textwrap import dedent
 
 from templateapp import version
 from templateapp import edition
@@ -101,6 +100,51 @@ def set_modal_dialog(dialog):
     dialog.wait_visibility()
     dialog.grab_set()
     dialog.wait_window()
+
+
+class Data:
+    license_name = 'BSD 3-Clause License'
+    repo_url = 'https://github.com/Geeks-Trident-LLC/templateapp'
+    license_url = path.join(repo_url, 'blob/main/LICENSE')
+    # TODO: Need to update wiki page for documentation_url instead of README.md.
+    documentation_url = path.join(repo_url, 'blob/develop/README.md')
+    copyright_text = 'Copyright @ 2021-2030 Geeks Trident LLC.  All rights reserved.'
+
+    @classmethod
+    def get_license(cls):
+        license_ = """
+            BSD 3-Clause License
+
+            Copyright (c) 2021, Geeks Trident LLC
+            All rights reserved.
+
+            Redistribution and use in source and binary forms, with or without
+            modification, are permitted provided that the following conditions are met:
+
+            1. Redistributions of source code must retain the above copyright notice, this
+               list of conditions and the following disclaimer.
+
+            2. Redistributions in binary form must reproduce the above copyright notice,
+               this list of conditions and the following disclaimer in the documentation
+               and/or other materials provided with the distribution.
+
+            3. Neither the name of the copyright holder nor the names of its
+               contributors may be used to endorse or promote products derived from
+               this software without specific prior written permission.
+
+            THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+            AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+            IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+            DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+            FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+            DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+            SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+            CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+            OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+            OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        """
+        license_ = dedent(license_).strip()
+        return license_
 
 
 class Application:
