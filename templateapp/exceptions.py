70774068f0072fc3e19b1f71352183b39507191b
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-08-23 20:08:18 -0700

    baseline: create exceptions.py

---
+++ templateapp/exceptions.py
@@ -0,0 +1,9 @@
+"""Module containing the exception class for templateapp."""
+
+
+class TemplateError(Exception):
+    """Use to capture error template construction."""
+
+
+class TemplateParsedLineError(TemplateError):
+    """Use to capture error parsed line for template builder."""
