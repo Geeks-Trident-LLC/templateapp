400b3794a76f77106500ac7557e6b7ae7d8aa101
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-09-01 23:39:12 -0700

    baseline: add TemplateBuilderInvalidFormat exception

--- templateapp/exceptions.py
+++ templateapp/exceptions.py
@@ -9,5 +9,9 @@ class TemplateParsedLineError(TemplateError):
     """Use to capture error parsed line for template builder."""
 
 
-class TemplateBuilderError(Exception):
+class TemplateBuilderError(TemplateError):
     """Use to capture error template construction."""
+
+
+class TemplateBuilderInvalidFormat(TemplateError):
+    """Use to capture error if user_data has invalid format."""
