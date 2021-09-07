b6b1f498fab35a2cb40b6aa00a7409fea9ce6e91
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-09-07 01:02:29 -0700

    version: update templateapp to version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='templateapp',
-    version='0.1.1',
+    version='0.1.2',
     license='BSD-3-Clause',
     license_files=['LICENSE'],
     description='The application to generate template format.',
