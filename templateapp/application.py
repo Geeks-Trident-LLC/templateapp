23d46fbd5e47b5af6479b32cedb52b903019812a
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-09-08 18:47:22 -0700

    gui: move Application.Data class to config.py

--- templateapp/application.py
+++ templateapp/application.py
@@ -5,9 +5,7 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 
-from os import path
 import webbrowser
-from textwrap import dedent
 from textwrap import indent
 import re
 import platform
@@ -23,6 +21,7 @@ from dlapp.collection import Tabular
 from templateapp import TemplateBuilder
 from templateapp.exceptions import TemplateBuilderInvalidFormat
 from templateapp.core import save_file
+from templateapp.config import Data
 
 from templateapp import version
 from templateapp import edition
@@ -117,51 +116,6 @@ def set_modal_dialog(dialog):
     dialog.wait_visibility()
     dialog.grab_set()
     dialog.wait_window()
-
-
-class Data:
-    license_name = 'BSD 3-Clause License'
-    repo_url = 'https://github.com/Geeks-Trident-LLC/templateapp'
-    license_url = path.join(repo_url, 'blob/main/LICENSE')
-    # TODO: Need to update wiki page for documentation_url instead of README.md.
-    documentation_url = path.join(repo_url, 'blob/develop/README.md')
-    copyright_text = 'Copyright @ 2021-2040 Geeks Trident LLC.  All rights reserved.'
-
-    @classmethod
-    def get_license(cls):
-        license_ = """
-            BSD 3-Clause License
-
-            Copyright (c) 2021-2040, Geeks Trident LLC
-            All rights reserved.
-
-            Redistribution and use in source and binary forms, with or without
-            modification, are permitted provided that the following conditions are met:
-
-            1. Redistributions of source code must retain the above copyright notice, this
-               list of conditions and the following disclaimer.
-
-            2. Redistributions in binary form must reproduce the above copyright notice,
-               this list of conditions and the following disclaimer in the documentation
-               and/or other materials provided with the distribution.
-
-            3. Neither the name of the copyright holder nor the names of its
-               contributors may be used to endorse or promote products derived from
-               this software without specific prior written permission.
-
-            THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-            AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-            IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-            DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-            FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-            DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-            SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-            CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-            OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-            OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        """
-        license_ = dedent(license_).strip()
-        return license_
 
 
 class Snapshot(dict):
@@ -648,7 +602,7 @@ class Application:
                 if self.search_chkbox_var.get():
                     self.search_chkbox.invoke()
 
-                if self.snapshot.curr_app == 'backup_app':
+                if self.snapshot.curr_app == 'backup_app':  # noqa
                     self.close_backup_btn.invoke()
 
                 content = stream.read()
@@ -675,7 +629,7 @@ class Application:
                 if self.search_chkbox_var.get():
                     self.search_chkbox.invoke()
 
-                if self.snapshot.curr_app == 'backup_app':
+                if self.snapshot.curr_app == 'backup_app':  # noqa
                     self.close_backup_btn.invoke()
 
                 content = stream.read()
@@ -849,7 +803,7 @@ class Application:
         scrollbar = ttk.Scrollbar(lframe, orient=tk.VERTICAL, command=txtbox.yview)
         scrollbar.grid(row=0, column=1, sticky='nsew')
         txtbox.config(yscrollcommand=scrollbar.set)
-        txtbox.insert(tk.INSERT, Data.get_license())
+        txtbox.insert(tk.INSERT, Data.license)
         txtbox.config(state=tk.DISABLED)
 
         # footer - copyright
@@ -1462,7 +1416,7 @@ class Application:
                 self.snapshot.update(is_built=True)
                 template = factory.template
             except Exception as ex:
-                template = self.snapshot.template.strip()
+                template = self.snapshot.template.strip()   # noqa
                 if not template:
                     error = '{}: {}'.format(type(ex).__name__, ex)
                     create_msgbox(title='RegexBuilder Error', error=error)
